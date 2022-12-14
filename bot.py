5e41b4f9609c1d4fd1fe31e048a872c138b366bb
udaydutt12 <udaydutt@utexas.edu>
2022-12-14 09:19:46 -0600

    initial commit

---
+++ bot.py
@@ -0,0 +1,108 @@
+import os
+
+from dotenv import load_dotenv
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.action_chains import ActionChains
+
+
+class ToneGeneratorBot:
+    def __init__(self, driver=None):
+        self._playing = False
+        self._website_url = os.environ.get('WEBSITE_URL')
+        if driver:
+            self._driver = driver
+        else:
+            chrome_options = webdriver.ChromeOptions()
+            chrome_options.add_experimental_option("excludeSwitches",
+                                ['enable-automation', 'enable-logging'])
+            chrome_options.headless = True
+            chromedriver_path = os.environ.get('CHROMEDRIVER_PATH')
+            service = Service(chromedriver_path)
+            self._driver = webdriver.Chrome(service=service, 
+                                        options=chrome_options)
+
+    def execute(self):
+        if self._playing:
+            print("press 1 to exit, press 2 to stop sound\npress any other key to continue")
+        else:
+            print("press 1 to exit, press any other key to continue")
+        x = input()
+        if x == '1':
+            return
+        if x == '2' and self._playing:
+            self._driver.find_element(By.ID, 'play-button').click()
+            self._playing = False
+            self.execute()
+            return
+        print("Enter a frequency between 1 and 20,154 Hz (will be rounded to 3 decimal places)")
+        f = float(input())
+        print("Enter an integer amplitude between 0 and 100%")
+        a = int(round(float(input())))
+        self._driver.get(f'{self._website_url}/tone#{f},v0.{a}')
+        self._driver.find_element(By.ID, 'play-button').click()
+        self._playing = True
+        self.execute()
+        
+        
+class ToneGeneratorBot2:
+    def __init__(self, driver=None):
+        self._playing = False
+        self._website_url = os.environ.get('WEBSITE_URL2')
+        if driver:
+            self._driver = driver
+        else:
+            chrome_options = webdriver.ChromeOptions()
+            chrome_options.add_experimental_option("excludeSwitches",
+                                ['enable-automation', 'enable-logging'])
+            #chrome_options.headless = True
+            chromedriver_path = os.environ.get('CHROMEDRIVER_PATH')
+            service = Service(chromedriver_path)
+            self._driver = webdriver.Chrome(service=service, 
+                                        options=chrome_options)
+        self._driver.implicitly_wait(10)
+        self._driver.get(self._website_url)
+
+    def execute(self):
+        if self._playing:
+            print("press 1 to exit, press 2 to stop sound\npress any other key to continue")
+        else:
+            print("press 1 to exit, press any other key to continue")
+        x = input()
+        print(type(x), x, x=='1')
+        if x == '1':
+            self._driver.close()
+            return
+        if x == '2' and self._playing:
+            self._driver.find_element(By.ID, 'play-button').click()
+            self._playing = False
+            self.execute()
+            return
+        print("Enter a frequency between 1 and 20,154 Hz (will be rounded to 3 decimal places)")
+        f = input()
+        print("Enter an integer amplitude between 0 and 100%")
+        a = int(round(float(input())))
+        # set the frequency
+        #self._driver.find_element(By.ID, 'freq-readout').click()
+        freq_text = self._driver.find_element(By.ID, 'freq-readout')
+        #element = self._driver.switch_to.active_element
+        #element.send_keys(Keys.CONTROL + 'a' + f)
+        #iinput = self._driver.find_element(By.CSS_SELECTOR, '#freq-readout input')
+        actions = ActionChains(self._driver)
+        actions.move_to_element(freq_text).click().send_keys(Keys.CONTROL, 'a', f).perform()
+        #actions.send_keys_to_element(Keys.CONTROL + 'a').send_keys_to_element(f).perform()
+        #print(iinput.text)
+        #iinput.text.replace("440", f)
+        #iinput.send_keys(Keys.CONTROL, 'a', Keys.BACKSPACE, f)
+        # set the amplitude
+        self._driver.find_element(By.ID, 'play-button').click()
+        
+        self._playing = True
+        self.execute()
+
+if __name__ == '__main__':
+    load_dotenv()
+    bot = ToneGeneratorBot()
+    bot.execute()
